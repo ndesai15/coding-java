b920508f552843415a6d4145623da4db193f9c4d
ndesai <neelharshadkumar.desai@here.com>
2022-03-27 15:07:34 -0500

    BoatsToSave

--- src/com/coding/patterns/greedy/BoatsToSavePeople.java
+++ src/com/coding/patterns/greedy/BoatsToSavePeople.java
@@ -9,6 +9,21 @@ import java.util.Arrays;
 // Time Complexity: O(N * logN)
 public class BoatsToSavePeople {
     public static int numberOfBoatsToSavePeople(int[] people, int limit) {
+        /*
+           [3, 2, 2, 1], limit = 3
+
+           In one boat only 2 persons are allowed. Weight of 2 has to be < limit.
+           Now if weight is less than we just need 1 boat if more than we will need to adjust one person
+           to next boat.
+
+           Let's be greedy. let's try to make a combination of heaviest and lightest persons.
+           Then use 2 pointers technique to see if we have a right combinations(< limit).
+
+           Sort the array: [1, 2, 2, 3], limit = 3
+           Use two pointers:[1, 2, 2, 3]
+                             ^        ^
+                             L        R
+         */
         Arrays.sort(people);
         int result = 0, left = 0, right = people.length - 1;
         while (left <= right) {
