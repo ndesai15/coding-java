7bd73d75a8651e2001ea33caff03fdff01d4f88a
Tushar Goyal <41803756+willingtushar@users.noreply.github.com>
2022-04-07 15:25:49 +0530

    Update TrappingRainingWater.java
    Added readable code

--- src/com/coding/patterns/twoPointers/TrappingRainingWater.java
+++ src/com/coding/patterns/twoPointers/TrappingRainingWater.java
@@ -1,43 +1,42 @@
-package com.coding.patterns.twoPointers;
-
-public class TrappingRainingWater {
-    public static int calculateTrappedWater(int[] height) {
-        int result = 0;
-        int left = 0;
-        int right = height.length - 1;
-        int maxLeft = height[left];
-        int maxRight = height[right];
-        left++;
-        right--;
-        while (left <= right) {
-            if (maxLeft <= maxRight) {
-                int diff = maxLeft - height[left];
-                if (diff > 0) {
-                    result += diff;
-                }
-                if (maxLeft < height[left]) {
-                    maxLeft = height[left];
-                }
-                left++;
-            }
-            else {
-                int diff = maxRight - height[right];
-                if (diff > 0) {
-                    result += diff;
-                }
-                if (maxRight < height[right]) {
-                    maxRight = height[right];
-                }
-                right--;
-            }
+class Solution {
+    public int trap(int[] height) {
+        // logic is not like largest rectangle in a histogram
+        // for a element at index i, here we need to find globalmax on left and globalmax on right
+        
+        int[] leftGlobalMax = getLeftMax(height);
+        int[] rightGlobalMax = getRightMax(height);
+        
+        int ans = 0;
+        for(int i=0;i<height.length;i++){
+            int leftMax = leftGlobalMax[i];
+            int rightMax = rightGlobalMax[i];
+            
+            int minHeight = Math.min(leftMax, rightMax);
+            int localAns = minHeight-height[i];
+            
+            ans += localAns;
         }
-        return result;
+        
+        return ans;
     }
-
-    public static void main(String[] args) {
-        int[] height1 = {4,2,0,3,2,5};
-        int[] height2 = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(calculateTrappedWater(height1));
-        System.out.println(calculateTrappedWater(height2));
+    
+    private int[] getLeftMax(int[] height){
+        int[] ans = new int[height.length];
+        
+        ans[0] = height[0];
+        for(int i=1;i<height.length;i++){
+            ans[i] = Math.max(ans[i-1], height[i]);
+        }
+        return ans;
+    }
+    
+    private int[] getRightMax(int[] height){
+        int[] ans = new int[height.length];
+        
+        ans[height.length-1] = height[height.length-1];
+        for(int i=height.length-2;i>=0;i--){
+            ans[i] = Math.max(ans[i+1], height[i]);
+        }
+        return ans;
     }
 }
