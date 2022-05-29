15eee5649ca0b010bbc0a32fb0f9b34c054c5961
michael-kang <mikexkang@gmail.com>
2022-05-29 18:54:48 -0400

    Update MaxAreaOfIsland.java

--- src/com/coding/patterns/dfs/MaxAreaOfIsland.java
+++ src/com/coding/patterns/dfs/MaxAreaOfIsland.java
@@ -1,10 +1,17 @@
-class Solution {
+/*
+      Leetcode link  :  https://leetcode.com/problems/max-area-of-island/
+	  Time Complexity:  O(M+N)
+	  Space Complexity: O(1)
+*/
+class MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
         
         int max = 0;
+        int numRows = grid.length;
+        int numCols = grid[0].length;
         
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numCols; j++){
                 max = Math.max(max, dfs(j, i, grid));
             }
         }
@@ -13,7 +20,9 @@ class Solution {
     }
     
     public int dfs(int x, int y, int[][] grid){
-        if(x < 0 || y < 0 || x >= grid[0].length || y >= grid.length || grid[y][x] == 0){
+        int numRows = grid.length;
+        int numCols = grid[0].length;
+        if(x < 0 || y < 0 || x >= numCols || y >= numRows || grid[y][x] == 0){
             return 0;
         }
         
